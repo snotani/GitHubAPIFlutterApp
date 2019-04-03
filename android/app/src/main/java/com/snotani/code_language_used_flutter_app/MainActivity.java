9bdf257b8f92e02f177ad99cc7647fcc6428c572
Siddharth Notani <36894543+snotani@users.noreply.github.com>
2019-04-04 00:26:43 +0100

    Initial commit

---
+++ android/app/src/main/java/com/snotani/code_language_used_flutter_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.snotani.code_language_used_flutter_app;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
