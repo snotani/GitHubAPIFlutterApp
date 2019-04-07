d0be64cf871dfe7cfb0c2302af8035a268774da9
Siddharth Notani <36894543+snotani@users.noreply.github.com>
2019-04-07 23:20:31 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,9 @@ class _MainState extends State<Main> {
       results.langSize = (resSize['Dart']/1000).toStringAsFixed(2);
       results.size = ((resBody['size']/100)*40).round();
     }
-    else isAvailable = false;
+    else {
+      isAvailable = false;
+    }
   }
 
   Widget build(BuildContext context) {
